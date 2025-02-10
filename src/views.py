6886e834eb831fb9a0c6db5008efcc3ac7495d51
Nex <markasch67@gmail.com>
2025-02-10 10:02:51 +0300

    Added logging for views

--- src/views.py
+++ src/views.py
@@ -1,12 +1,32 @@
+import os
+import logging
 import json
 from functools import wraps
 from typing import Any, Callable
 
 
+log_path = "../logs/views.log"
+
+# Устраняет ошибку отсутствия файла при импорте модуля
+if str(os.path.dirname(os.path.abspath(__name__)))[-3:] != "src":
+    log_path = log_path[1:]
+
+
+logger = logging.getLogger("views")
+file_handler = logging.FileHandler(log_path, "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
+
+
 def create_report(data: dict | list[dict], file_path: str) -> None:
     """Записывает информацию в JSON-файл."""
-    with open(file_path, "w", encoding="utf-8") as f:
-        json.dump(data, f, indent=4, ensure_ascii=False)
+    try:
+        with open(file_path, "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=4, ensure_ascii=False)
+    except Exception as e:
+        logger.critical(f"Произошла ошибка при записи в файл: {e.reason}")
 
 
 def save_report(filename: str = "") -> Callable:
@@ -22,10 +42,11 @@ def save_report(filename: str = "") -> Callable:
             try:
                 result = func(*args, **kwargs)
             except Exception as e:
-                result = f"{func.__name__} executed with an error: {e}. "
+                logger.critical(f"Произошла ошибка при выполнении функции {func.__name__}: {e.reason}"
 
             # Запись в файл
             if filename == "":
+                logger.info("Название файла не задано, запись в стандартный файл")
                 create_report(result, "../output/report_result.json")
             else:
                 create_report(result, filename)
