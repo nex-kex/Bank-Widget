a7f503aadd49c9a086192f8206c5ff93f5260106
Nex <markasch67@gmail.com>
2025-02-10 07:20:40 +0300

    Added logging to services

--- src/services.py
+++ src/services.py
@@ -1,6 +1,23 @@
+import os
+import logging
 from collections import defaultdict
 
 from src.sorting import sort_by_period
+
+
+log_path = "../logs/services.log"
+
+# Устраняет ошибку отсутствия файла при импорте модуля
+if str(os.path.dirname(os.path.abspath(__name__)))[-3:] != "src":
+    log_path = log_path[1:]
+
+
+logger = logging.getLogger("services")
+file_handler = logging.FileHandler(log_path, "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
 
 
 def get_category_cashback(transactions_list: list[dict]) -> dict:
@@ -8,9 +25,13 @@ def get_category_cashback(transactions_list: list[dict]) -> dict:
     category_cashback: dict = defaultdict(int)
 
     for transaction in transactions_list:
-        if transaction.get("Категория") and str(transaction["Кэшбэк"]) != "nan":
-            category_cashback[transaction["Категория"]] += transaction["Кэшбэк"]
+        if transaction.get("Категория") and str(transaction["Категория"]) != "nan":
+            if transaction.get("Кешбэк") and str(transaction["Кэшбэк"]) != "nan":
+                category_cashback[transaction["Категория"]] += transaction["Кэшбэк"]
 
+    if len(category_cashback) == 0:
+        logger.warning("За текущий период не получено кешбэка по найденным категориям.")
+    
     # Сортировка названий категорий по убыванию суммы кешбэка в них
     sorted_categories = sorted(category_cashback, key=lambda x: category_cashback[x], reverse=True)
 
@@ -27,11 +48,15 @@ def count_investments(transactions_list: list[dict], limit: int) -> float:
 
     investments = 0.0
 
-    for transaction in transactions_list:
-        # Проверка на то, что это расход и он был успешно выполнен
-        if transaction["Статус"] == "OK" and transaction["Сумма операции"] < 0:
-            investments += transaction["Сумма операции"] % limit
+    try:
+        for transaction in transactions_list:
+            # Проверка на то, что это расход и он был успешно выполнен
+            if transaction["Статус"] == "OK" and transaction["Сумма операции"] < 0:
+                investments += transaction["Сумма операции"] % limit
 
+    except KeyError as e:
+        logger.critical(f"Получена транзакция без необходимого ключа: {e}")
+    
     return investments
 
 
