0d5a86a396c58a4576483828c15555aafd084f8a
Nex <markasch67@gmail.com>
2025-02-10 10:06:23 +0300

    Added logging for utils

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,30 @@
+import os
+import logging
 import pandas as pd
+
+
+log_path = "../logs/utils.log"
+
+# Устраняет ошибку отсутствия файла при импорте модуля
+if str(os.path.dirname(os.path.abspath(__name__)))[-3:] != "src":
+    log_path = log_path[1:]
+
+
+logger = logging.getLogger("utils")
+file_handler = logging.FileHandler(log_path, "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
 
 
 def read_from_xlsx(xlsx_file: str) -> list[dict]:
     """Читает XLSX-файл file_name с транзакциями и возвращает их в виде списка словарей."""
-    data = pd.read_excel(xlsx_file)
-    transactions = data.to_dict(orient="records")
+    try:
+        data = pd.read_excel(xlsx_file)
+        transactions = data.to_dict(orient="records")
+        
+    except Exception as e:
+        logger.critical(f"Произошла ошибка при чтении XLSX-файла: {e.reason}")
+
     return transactions
